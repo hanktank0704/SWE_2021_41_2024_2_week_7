3dbecc2e8e1c59c76b37bf33314323bf4f2e0b60
hanktank0704 <hanktank0704@naver.com>
2024-10-16 19:13:10 +0900

    main change

---
+++ main.py
@@ -0,0 +1,7 @@
+from typing import List
+def twoSum(nums: List[int], target: int) -> List[int]:
+    # Your Codes
+    ## Do not use input() or print() in your function
+    ## Inputs (nums, target) will given as arguments and the output as
+    ## return value
+    return # Your Answer
